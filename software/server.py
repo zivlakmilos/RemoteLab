77b3432365bcfb02743a5f83a12d4280df05d073
Milos Zivlak <zivlakmilos@gmail.com>
2016-11-04 14:03:34 +0100

    Code for digital (on/off) led-s, server and client test

--- software/server.py
+++ software/server.py
@@ -2,13 +2,52 @@
 
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from communication import Communication
 
 app = Flask(__name__);
+app.config["SECRET_KEY"] = "RemoteLab";
+
+socket = SocketIO(app);
+
+port = Communication();
+port.open("/dev/ttyUSB0");
 
 @app.route("/")
 def index():
     return render_template("index.html");
 
+@socket.on("connect")
+def clientConnected():
+    updateState();
+
+@socket.on("setLedState")
+def setLedState(led, state):
+    if state.lower() == "on":
+        port.ledOn(led);
+    else:
+        port.ledOff(led);
+    updateState();
+
+@socket.on("setLedPwm")
+def setLedPwm(pwm):
+    port.ledPwm(pwm);
+    updateState();
+
+def updateState():
+    led1 = "On";
+    led2 = "On";
+    led3 = "\x00";
+
+    if port.ledState("led1"):
+        led1 = "Off";
+
+    if port.ledState("led2"):
+        led2 = "Off";
+
+    socket.emit("updateState", { "led1": led1,
+                                 "led2": led2,
+                                 "led3": led3});
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001);
+    socket.run(app);
 
